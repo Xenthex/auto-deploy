a1663194936d068aba6d1ccd3204dd4e1e5e4e4c
Xenthex <Xenthex@users.noreply.github.com>
2019-02-03 00:20:40 +1000

    Update auto.cpp

--- auto.cpp
+++ auto.cpp
@@ -40,18 +40,19 @@ int main(int argc, char const *argv[])
     { 
         perror("bind failed"); 
         exit(EXIT_FAILURE); 
-    } 
+    }
     if (listen(server_fd, 3) < 0) 
     { 
         perror("listen"); 
         exit(EXIT_FAILURE); 
-    } 
+    }
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
                        (socklen_t*)&addrlen))<0) 
     { 
         perror("accept"); 
         exit(EXIT_FAILURE); 
-    } 
+    }
+    printf("Awaiting connection...");
     valread = read( new_socket , buffer, 1024); 
     printf("%s\n",buffer ); 
     send(new_socket , hello , strlen(hello) , 0 ); 
